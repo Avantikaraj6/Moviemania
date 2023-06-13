2f75df80852cd27364196f22196b4e8a3ad3a985
Avantikaraj6 <126805949+Avantikaraj6@users.noreply.github.com>
2023-06-13 13:11:57 +0530

    Avni creation

--- src/components/Header.js
+++ src/components/Header.js
@@ -59,7 +59,7 @@ const Header = (props) => {
   return (
     <Nav>
       <Logo>
-        <img src="/images/logo.svg" alt="Disney+" />
+        <img src="/images/unnamed.jpg" alt="Disney+" />
       </Logo>
 
       {!userName ? (
